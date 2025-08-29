2390a2d2d900359260f6457a938361594df978df
BrunoSilvestre00 <bruno.ap.silvestre@gmail.com>
2025-08-29 16:01:21 -0300

    feat: separate endpoints

--- bin/endpoints.R
+++ bin/endpoints.R
@@ -9,16 +9,97 @@ function() {
   )))
 }
 
-#* @post /start-assessment
+#* @post /irt/start-assessment
+function(req) {
+  # - questions: list of questions with parameters
+  # - config: elements which goes into the design of the assessment
+  #     - model: model type (3PL, 2PL, 1PL) item_type input
+  #     - start_item: index of the first item to be presented
+  #     - criteria: next item selection criteria
+  #     - min_sem: minimum standard error of measurement
+  #     - delta_thetas: change in theta for stopping criteria
+  #     - thetas_start: initial theta values
+  #     - pattern_theta: pattern of theta values for multidimensional assessments
+  #     - min_items: minimum number of items to be administered
+  #     - max_items: maximum number of items to be administered
+  #     - max_time: maximum time allowed for the assessment
+
+  questions <- req$body$questions
+  config <- req$body$config
+
+  model <- config$model # 3PL
+  start_item <- config$start_item
+  criteria <- config$criteria
+
+  min_sem <- config$min_sem
+  delta_thetas <- config$delta_thetas
+  thetas_start <- config$thetas_start
+  pattern_theta <- config$pattern_theta
+
+  min_items <- config$min_items
+  max_items <- config$max_items
+  max_time <- ifelse(
+    !is.null(config$max_time),
+    config$max_time,
+    Inf
+  )
+
+  design <- list(
+    min_SEM = min_sem,
+    delta_thetas = delta_thetas,
+    thetas.start = thetas_start,
+    min_items = min_items,
+    max_items = max_items,
+    max_time = max_time
+  )
+
+  # create mirt object
+  irt_params <- build_irt_parameters(
+    discrimination_list = questions$discrimination,
+    difficulty_list = questions$difficulty,
+    guessing_list = questions$guess
+  )
+  
+  mo <- create_mirt_object(
+    item_type = ifelse(
+      model %in% list("3PL", "2PL", "1PL"),
+      model, 
+      "3PL"
+    ),
+    parameters = irt_params,
+    latent_covariance = matrix(2)
+  )
+  
+  # start assessment
+  cat_design <- create_cat_design(
+    mo, 
+    pattern_theta = pattern_theta, 
+    criteria = criteria, 
+    start_item = start_item,
+    design = design 
+  )
+
+  cat_design$item_time_history <- list()
+  cat_design$last_answer_time <- Sys.time()
+  
+  next_index <- mirtCAT::findNextItem(cat_design)
+  
+  return(list(
+    next_index = jsonlite::unbox(next_index),
+    stop = jsonlite::unbox(cat_design$design@stop_now),
+    design = jsonlite::unbox(serialize_design(cat_design))
+  ))
+}
+
+
+#* @post /cdm/start-assessment
 function(req) {
   # req contains the request object with elements:
   #   - questions: list of questions with parameters
-  #   - q_matrix: Q-matrix for CDM (if applicable)
-  #   - assessment_type: type of assessment (TRI or MDC)
-  #   - model: model type (IRT(3PL, 2PL, 1PL) or CDM(DINA, DINO, GDINA))
+  #   - q_matrix: Q-matrix for CDM
   #   - config: elements which goes into the design of the assessment
+  #       - model: model type (DINA, DINO, GDINA) item_type input
   #       - start_item: index of the first item to be presented
-  #       - type: model type (IRT or CDM) item_type input
   #       - criteria: next item selection criteria
   #       - method: estimation method
   #       - min_sem: minimum standard error of measurement
@@ -29,19 +110,14 @@ function(req) {
   #       - max_items: maximum number of items to be administered
   #       - max_time: maximum time allowed for the assessment
 
-  print("Starting assessment...")
   # request arguments
   questions <- req$body$questions
+  q_matrix <- req$body$q_matrix
   config <- req$body$config
 
-  assessment_type <- req$body$assessment_type # IRT or CDM
-  model <- req$body$model # model type IRT(3PL, 2PL, 1PL) or CDM(DINA, DINO, GDINA)
-  q_matrix <- req$body$q_matrix # Q-matrix for CDM
-
   # assessment arguments that goes into the design
-  item_type <- config$item_type # model type IRT(3PL, 2PL, 1PL) or CDM(DINA, DINO, GDINA)
+  model <- config$model
   start_item <- config$start_item
-  criteria <- config$criteria # next item selection criteria
   method <- config$method # estimation method
   min_sem <- config$min_sem
   delta_thetas <- config$delta_thetas
@@ -63,68 +139,86 @@ function(req) {
     min_items = min_items,
     max_items = max_items,
     max_time = max_time,
-    customUpdateThetas = if(assessment_type == "MDC") customUpdateThetas else NULL, # CDM
-    customNextItem = if(assessment_type == "MDC") customNextItem else NULL # CDM
+    customUpdateThetas = customUpdateThetas,
+    customNextItem = customNextItem
   )
 
   # create mirt object
-  if(assessment_type == "TRI"){
-    params <- build_irt_parameters(
-      discrimination_list = questions$discrimination,
-      difficulty_list = questions$difficulty,
-      guessing_list = questions$guess
-    )
-    trait_cov <- matrix(2) # check
-  }else if(assessment_type == "MDC") {
-    source("mirtCAT.R") # edit some mirtCAT objects
-    params <- generate_fake_mirt_pars(q_matrix)
-    trait_cov <- diag(ncol(q_matrix))
-    cdm_parameters <- questions
-    
-  }else{ # CAT com TRI e MDC
-  }
+  source("mirtCAT.R") # edit some mirtCAT objects
+  params <- generate_fake_mirt_pars(q_matrix)
+  trait_cov <- diag(ncol(q_matrix))
+  cdm_parameters <- questions
     
   mo <- create_mirt_object(
-    item_type = ifelse( # remove this when implementing CDM
-      item_type %in% list("3PL", "2PL", "1PL"),
-      item_type, 
-      "3PL"
-    ),
+    item_type = model,
     parameters = params,
     latent_covariance = trait_cov  # Multidimensional element (validate importance)
   )
-  
-
 
   # start assessment
   cat_design <- create_cat_design(
     mo, 
     pattern_theta = pattern_theta, 
-    criteria = if(assessment_type == "MDC") "custom" else criteria,
+    criteria = "custom",
     method = method,
     start_item = start_item,
-    design = design #,q_matrix = q_matrix #  CDM TEST 
+    design = design
   )
 
   cat_design$item_time_history <- list()
   cat_design$last_answer_time <- Sys.time()
   
-  # next_index <- mirtCAT::findNextItem(cat_design)
   next_index <- cat_design$design@start_item
 
   return(list(
     next_index = jsonlite::unbox(next_index),
-    stop = jsonlite::unbox(cat_design$design@stop_now),
-    assessment_type = jsonlite::unbox(assessment_type), # NEW
-    criteria = jsonlite::unbox(criteria), # NEW
-    model = jsonlite::unbox(serialize_design(model)), # NEW
-    questions = jsonlite::unbox(serialize_design(questions)), # NEW
-    q_matrix = jsonlite::unbox(serialize_design(q_matrix)), # NEW
+    model = jsonlite::unbox(serialize_design(model)),
+    questions = jsonlite::unbox(serialize_design(questions)),
+    q_matrix = jsonlite::unbox(serialize_design(q_matrix)),
     design = jsonlite::unbox(serialize_design(cat_design))
   ))
 }
 
-#* @post /next-item
+
+#* @post /irt/next-item
+function(req) {
+  e_design <- req$body$design
+  answer <- req$body$answer
+  prev_item <- req$body$previous_index
+  
+  # deserialize and update design
+  cat_design <- mirtCAT::updateDesign(
+    deserialize_design(e_design),
+    new_item = prev_item,
+    new_response = answer,
+    updateTheta = TRUE
+  )
+
+  now <- Sys.time()
+  cat_design$item_time_history <- append(
+    cat_design$item_time_history,
+    as.numeric(difftime(
+      now, cat_design$last_answer_time, units = "secs"
+    ))
+  )
+  cat_design$last_answer_time <- now
+
+  # get next item
+  next_index <- ifelse(
+    !cat_design$design@stop_now,
+    mirtCAT::findNextItem(cat_design),
+    0
+  )
+  
+  return(list(
+    next_index = jsonlite::unbox(next_index),
+    stop = jsonlite::unbox(cat_design$design@stop_now),
+    design = jsonlite::unbox(serialize_design(cat_design))
+  ))
+}
+
+
+#* @post /cdm/next-item
 function(req) {
   
   # request arguments
@@ -132,18 +226,16 @@ function(req) {
   model <- deserialize_design(req$body$model)
   questions <- deserialize_design(req$body$questions)
   q_matrix <- deserialize_design(req$body$q_matrix)
-  assessment_type <- req$body$assessment_type
   criteria <- req$body$criteria
   answer <- req$body$answer
   prev_item <- req$body$previous_index
   
   # set CDM variables to global environment
-  if(assessment_type == "MDC"){
-    model <<- model
-    criteria <<- criteria
-    cdm_parameters <<- questions
-    q_matrix <<- q_matrix
-  }
+  model <<- model
+  criteria <<- criteria
+  cdm_parameters <<- questions
+  q_matrix <<- q_matrix
+
   # deserialize and update design
   cat_design <- mirtCAT::updateDesign(
     e_design,
@@ -161,30 +253,23 @@ function(req) {
   )
   cat_design$last_answer_time <- now
 
-  # get next item
-  
-#   next_index <- ifelse(
-#     !cat_design$design@stop_now,
-#     mirtCAT::findNextItem(cat_design,  objective = objective),
-#     0
-#   )
   if(cat_design$design@stop_now){
     next_index <- 0
-  }else if(assessment_type == "TRI"){
-    next_index <- mirtCAT::findNextItem(cat_design)
-  }else if(assessment_type == "MDC"){
-    next_index <- customNextItem(person = cat_design$person, design = cat_design$design, test = cat_design$test)
+  } else {
+    next_index <- customNextItem(
+      person = cat_design$person,
+      design = cat_design$design,
+      test = cat_design$test
+    )
   }
   
-
   return(list(
     next_index = jsonlite::unbox(next_index),
     stop = jsonlite::unbox(cat_design$design@stop_now),
-    assessment_type = jsonlite::unbox(assessment_type), # NEW
-    criteria = jsonlite::unbox(criteria), # NEW
-    model = jsonlite::unbox(serialize_design(model)), # NEW
-    questions = jsonlite::unbox(serialize_design(questions)), # NEW
-    q_matrix = jsonlite::unbox(serialize_design(q_matrix)), # NEW
+    criteria = jsonlite::unbox(criteria),
+    model = jsonlite::unbox(serialize_design(model)),
+    questions = jsonlite::unbox(serialize_design(questions)),
+    q_matrix = jsonlite::unbox(serialize_design(q_matrix)),
     design = jsonlite::unbox(serialize_design(cat_design))
   ))
 }
--- main.R
+++ main.R
@@ -5,4 +5,5 @@ app_version <- "1.0.0"
 source("utils/serializer.R")
 source("bin/env.R")
 source("bin/cat.R")
+source("bin/mdc.R")
 source("bin/plumber.R")
